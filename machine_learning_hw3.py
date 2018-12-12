a7b2b50e4043a30a53abd4cdfe3b998f8da7e907
jackma-ME <jackma010039@gmail.com>
2018-12-12 14:39:57 +0800

    [fixed]try fix error

--- machine_learning_hw3.py
+++ machine_learning_hw3.py
@@ -9,6 +9,12 @@ x_length = 12
 y_length = 4
 
 def observe(x,y,a):
+'''
+	ran = random.random()
+	if ran < 0.2:
+		ran2 = random.randint(1,3)
+		a = (a + ran2) % 4
+'''
 	goal = 0
 	if x == x_length - 1 and y == 0:
 		goal = 1
@@ -26,7 +32,7 @@ def observe(x,y,a):
 	y = min(y_length - 1, y)
 	
 	if goal == 1:
-		return x,y,500
+		return x,y,1
 	elif x > 0 and x < x_length-1 and y == 0:
 		return 0,0,-100
 	return x,y,-1
@@ -102,12 +108,15 @@ def epsilon_policy(x,y,q,eps):
 				q_max = q[x][y][i]
 				a_max = i
 		a = a_max
+	return a
+'''
 	ran = random.random()
 	if ran < 0.2:
 		a = valid_action(x,y,a,ran)
 		return a
 	else:
-		return a 
+		return a
+''' 
 
 def max_q(x,y,q):
 	q_max = q[x][y][0]
